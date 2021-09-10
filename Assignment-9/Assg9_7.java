f517142343c2ecf4cf4cb8a0b33adf0fbf7f3b07
Kishore2681999 <89398887+Kishore2681999@users.noreply.github.com>
2021-09-10 07:43:36 +0530

    Add files via upload

---
+++ Assignment-9/Assg9_7.java
@@ -0,0 +1,35 @@
+package Assignment-9;
+
+public class Assg9_7 {
+	private final int newsId;
+	private final String postedByUser;
+	private final String commentByUser;
+	private final String comment;
+	public news(int newsId, String postedByUser, String commentByUser, String comment) {
+		
+		this.newsId = newsId;
+		this.postedByUser = postedByUser;
+		this.commentByUser = commentByUser;
+		this.comment = comment;
+	}
+	public int getNewsId() {
+		return newsId;
+	}
+	public String getPostedByUser() {
+		return postedByUser;
+	}
+	public String getCommentByUser() {
+		return commentByUser;
+	}
+	public String getComment() {
+		return comment;
+	}
+	@Override
+	public String toString() {
+		return "news [newsId=" + this.newsId + ", postedByUser=" + this.postedByUser + ", commentByUser=" + this.commentByUser
+				+ ", comment=" + this.comment + "]";
+	}
+	
+	
+	
+}
