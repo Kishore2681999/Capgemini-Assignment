ab4586d1b22606e9a5e6a4fa8ae9756078afacb4
Kishore2681999 <89398887+Kishore2681999@users.noreply.github.com>
2021-09-10 06:32:09 +0530

    Add files via upload

---
+++ Assignment-7/Assg7_1.java
@@ -0,0 +1,30 @@
+package Assignment-7;
+
+import java.lang.annotation.ElementType;
+//import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
+
+public class Assg7_1 {
+
+	// this is to create a custom annotation
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.RUNTIME)
+	@interface Test{
+		
+	}
+	
+	public static void main(String[] args) {
+		printSeries(10);
+	}
+
+	@Test
+	private static void printSeries(int i) {
+		// TODO Auto-generated method stub
+		int n=10;
+		while(n!=0) {
+			System.out.println(n);
+			n--;
+		}
+		
+	}
+}
